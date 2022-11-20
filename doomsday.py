f51694669dbe85f7b0810d0a16666927fd3e6eb8
M.E <52017620+acroscopic@users.noreply.github.com>
2022-11-20 11:04:59 -0600

    Update doomsday.py

--- doomsday.py
+++ doomsday.py
@@ -1,3 +1,9 @@
+
+
+#This program was made to practice John Conway's Doomsday Algorithim (https://en.wikipedia.org/wiki/Doomsday_rule) 
+#It loops random dates, and only breaks the loop when an attempt is failed. It also features a simple high score tracker, and a timer.
+
+
 import shelve
 import random
 import calendar
